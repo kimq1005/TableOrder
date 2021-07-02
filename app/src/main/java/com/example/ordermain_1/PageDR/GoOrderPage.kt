7b9987a561e81ccfea938cf4481c981b871d5e01
DESKTOP-OIKRFTK\user <tpwnd103502@naver.com>
2021-07-02 18:23:34 +0900

    7월 2일 오후 6시 커밋

--- app/src/main/java/com/example/ordermain_1/PageDR/GoOrderPage.kt
+++ app/src/main/java/com/example/ordermain_1/PageDR/GoOrderPage.kt
@@ -3,6 +3,7 @@ package com.example.ordermain_1.PageDR
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import com.bumptech.glide.Glide
 import com.example.ordermain_1.Item.RealMenuItem
 import com.example.ordermain_1.R
@@ -16,14 +17,55 @@ class GoOrderPage : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_go_order_page)
 
-        val  name = intent.getStringExtra("no")
+        val menu_name = intent.getStringExtra("menu_name")
+        val menu_info = intent.getStringExtra("menu_information")
+        val menu_price = intent.getStringExtra("menu_price")
+//        val menu_img = intent.getStringExtra("menu_img")
+        //
 
-        tv_name.text= name
+
+
+//        Glide.with(this).load(menu_img).into(menu_img)
+        OrderPage_menuName_txt.text= menu_name
+        OrderPage_menuinfo_txt.text =menu_info
+        OrderPage_menuPrice_txt.text  = menu_price
+
+        menuBtnClick()
 
 
 
 
 
+
+
+
+    }
+
+    private fun menuBtnClick() {
+
+        var sum:Int = 0
+
+         PlusBtn.setOnClickListener {
+             for(i in 1..1){
+                 sum=sum+1
+         }
+
+             ZeroBtn.text = sum.toString()
+
+        }
+
+        MinusBtn.setOnClickListener {
+            for(i in 1..1){
+                sum=sum-1
+            }
+
+            ZeroBtn.text = sum.toString()
+
+            if(sum<0){
+                sum = 0
+                ZeroBtn.text = sum.toString()
+            }
+        }
 
     }
 }
