3da20267d531387dad4f8650cf84a2a1cdcd7661
kimq1005 <tpwnd103502@naver.com>
2021-07-03 18:49:23 +0900

    메뉴 주문 페이지(7월 3일)

--- app/src/main/java/com/example/ordermain_1/PageDR/GoOrderPage.kt
+++ app/src/main/java/com/example/ordermain_1/PageDR/GoOrderPage.kt
@@ -3,6 +3,7 @@ package com.example.ordermain_1.PageDR
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Toast
 import com.bumptech.glide.Glide
 import com.example.ordermain_1.Item.RealMenuItem
@@ -20,12 +21,15 @@ class GoOrderPage : AppCompatActivity() {
         val menu_name = intent.getStringExtra("menu_name")
         val menu_info = intent.getStringExtra("menu_information")
         val menu_price = intent.getStringExtra("menu_price")
+//        val menu_img = intent.getStringArrayExtra("menu_img")
+
 //        val menu_img = intent.getStringExtra("menu_img")
         //
 
 
 
-//        Glide.with(this).load(menu_img).into(menu_img)
+
+//        Glide.with(this).load(menu_img).into(OrderPage_menuImg_img)
         OrderPage_menuName_txt.text= menu_name
         OrderPage_menuinfo_txt.text =menu_info
         OrderPage_menuPrice_txt.text  = menu_price
